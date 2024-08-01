35b7dfbaac67b154c95c5902ac8b1a332e6e1dd2
darky623 <kir.taraso@gmail.com>
2024-08-01 18:04:37 +0200

    Create project

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 sqlite_database = "sqlite:///auth.db"
+
+secret_key = "secret_key"
+
+token_lifetime = 3600
+
+webhook_port = 80
 webhook_ssl_cert = None
 webhook_ssl_priv = None
-webhook_port = 80
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -7,13 +7,35 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     email = Column(String)
-    password = Column(String)
-    status = Column(String)
-    reg_date = Column(DateTime)
-    last_login = Column(DateTime)
+    user_hash = Column(String)
+    status = Column(String, default='active')
+    create_date = Column(DateTime)
+    auth_sessions = relationship("AuthSession", back_populates="user")
 
+
+class AuthSession(Base):
+    __tablename__ = "auth_sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="auth_sessions")
+    token = Column(String)
+    status = Column(String, default='active')
+    create_date = Column(DateTime)
+
+
+class Server(Base):
+    __tablename__ = "servers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String)
+    name = Column(String)
+    locale = Column(String)
+    max_players = Column(String)
+    status = Column(String)
+    create_date = Column(DateTime)
