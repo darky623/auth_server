bf12d56749773d520520ef7cd78163a36a1ca4af
Tarasov Kirill <59022237+darky623@users.noreply.github.com>
2024-10-31 17:23:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ secret_key = "secret_key"
 
 token_lifetime = 3600
 
-webhook_port = 80
-webhook_ssl_cert = None
-webhook_ssl_priv = None
+webhook_port = 443
+webhook_ssl_cert = "/etc/letsencrypt/live/lotw-auth.clava.space/fullchain.pem"
+webhook_ssl_priv = "/etc/letsencrypt/live/lotw-auth.clava.space/privkey.pem"
