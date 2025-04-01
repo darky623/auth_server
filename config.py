be1844a97c63e3915a5f5f852f3a82f6d7fb383c
Tarasov Kirill <59022237+darky623@users.noreply.github.com>
2025-04-01 20:24:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ sqlite_database = "sqlite+aiosqlite:///auth.db"
 
 secret_key = "secret_key"
 
+allow_user_registration = True
+
+min_build_version = "1.1.5"
+
 token_lifetime = 3600
 
 webhook_port = 443
